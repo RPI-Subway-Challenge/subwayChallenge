e2511d54bb6368f5e1bfeb00ac34c4f05f2e0ab0
Mason duBoef <mduboef@gmail.com>
2022-02-17 10:18:46 -0500

    Dedug algo file

--- algos.cpp
+++ algos.cpp
@@ -1,27 +1,23 @@
 //																	D O W N    W I T H    M A T T H E W    A H N
 #include <iostream>
-#include <utility>	// pair
-#include <string>
-#include <vector>
-#include <list>
 #include <cmath>
-
+#include "station.h"
 
 
 // return manhattan distance between two stations
 double manhattanDistance(Station s1, Station s2){
-    double diffX = std::abs( s1.getCords().first - s2.getCords().first )
-    double diffY = std::abs( s1.getCords().second - s2.getCords().second )
-    return diffX+diffY
+    double diffX = std::abs( s1.getCords().first - s2.getCords().first );
+    double diffY = std::abs( s1.getCords().second - s2.getCords().second );
+    return diffX+diffY;
 }
 
 
 
 // return actual distance between two stations using pythagorean theorem
 double realDistance(Station s1, Station s2){
-    double diffX = std::abs( s1.getCords().first - s2.getCords().first )
-    double diffY = std::abs( s1.getCords().second - s2.getCords().second )
-    return std::sqrt( std::pow(diffX,2) + std::pow(diffX,2) )
+    double diffX = std::abs( s1.getCords().first - s2.getCords().first );
+    double diffY = std::abs( s1.getCords().second - s2.getCords().second );
+    return std::sqrt( std::pow(diffX,2) + std::pow(diffX,2) );
 }
 
 
@@ -31,7 +27,7 @@ double realDistance(Station s1, Station s2){
 std::list<int> walkableRadius(int stationIndex, double radius, std::vector<Station> & stationVec){
     std::list<int> walkable;
     for(int i = 0; i != stationVec.size(); i++){
-        if(realDistance(stationIndex, stationVec[i]) <= radius && i != stationIndex){
+        if(realDistance(stationVec[stationIndex], stationVec[i]) <= radius && i != stationIndex){
             walkable.push_back(i);
         }
     }
@@ -41,26 +37,26 @@ std::list<int> walkableRadius(int stationIndex, double radius, std::vector<Stati
 
 
 // takes vector of pairs walkable and returns index with longest distance
-int findLongest(std::vector<std::pair<int,double]>> walkable){
-    longest = 0;
+int findLongest(std::vector<std::pair<int,double>> walkable){
+    int longest = 0;
     for(int i = 1; i != walkable.size(); i++){
         if(walkable[i].second > walkable[longest].second){ longest = i; }
     }
-    return longest
+    return longest;
 }
 
 
 
 // takes station returns vector of x nearest stations indicies
 // O(x*num of stations) for single station
-std::vector<int> walkableNumber(int stationIndex, int x, std::vector<Station> & stationVec){
+std::vector<std::pair<int,double>> walkableNumber(int stationIndex, int x, std::vector<Station> & stationVec){
 
-    if(x < 1){std::cerr << "ERROR: X in walkableNumber must be >= 1\n"; return NULL;}
-
-    if(x >= stationVec.size()){std::cerr << "ERROR: X in walkableNumber must be < stationVec.size()\n"; return NULL;}
-
-    std::vector<std::pair<int,double>> walkable(x)       // pair < neighborIndex, distance >
+    std::vector<std::pair<int,double>> walkable(x);      // pair < neighborIndex, distance >
     int longest = 0;                                     // index in walkable of farthest away neighbor
+
+    if(x < 1){std::cerr << "ERROR: X in walkableNumber must be >= 1\n"; return walkable;}
+
+    if(x >= stationVec.size()){std::cerr << "ERROR: X in walkableNumber must be < stationVec.size()\n"; return walkable;}
 
     // put first x stations in walkable
     for(int i = 0; i != x; i++){
@@ -70,22 +66,23 @@ std::vector<int> walkableNumber(int stationIndex, int x, std::vector<Station> & 
             longest = i;
         // not stationIndex
         }else{
-            walkable[i] = std::make_pair(i, realDistance(stationIndex,stationVec[i]));
+            walkable[i] = std::make_pair(i, realDistance(stationVec[stationIndex],stationVec[i]));
             // check against longest
-            if(realDistance(stationIndex,stationVec[i]) > walkable[longest].second){
+            if(realDistance(stationVec[stationIndex],stationVec[i]) > walkable[longest].second){
                 longest = i;
             }
         }
     }
 
     // check stations from index x to end
-    for(int i = x; i != stationsVec.size(); i++){
+    for(int i = x; i != stationVec.size(); i++){
         if(i != stationIndex){
             // check against longest
-            if( realDistance(stationIndex,stationVec[i]) > walkable[longest].second ){
-                walkable[longest] = std::make_pair(i, realDistance(stationIndex,stationVec[i]));
+            if( realDistance(stationVec[stationIndex],stationVec[i]) > walkable[longest].second ){
+                walkable[longest] = std::make_pair(i, realDistance(stationVec[stationIndex],stationVec[i]));
                 longest = findLongest(walkable);
             } 
+        }
     }
     return walkable;
 }

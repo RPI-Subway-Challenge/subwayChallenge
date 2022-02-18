5762fe643d0e95914e8d5f1c3489f15305e9d2a0
Mason duBoef <mduboef@gmail.com>
2022-02-18 17:38:38 -0500

    Operator declarations

--- time.h
+++ time.h
@@ -11,14 +11,19 @@ public:
 	Time();
 	// constructor
 	Time(int inputHour, int inputMin);
-    //String constructor
+    // string constructor
     Time(const std::string& inputHour, const std::string& inputMin);
-
     Time(const std::string& input);
 	// getters
     unsigned int getHour()	{return min;}
     unsigned int getMin()	{return (int)hour;}
     double getMinSec() {return min;}
+	// operators
+	// Time operator + (Time xTime);
+	// Time operator - (Time xTime);
+	// void operator = (Time xTime);
+	// bool operator > (Time xTime);
+
 private:
 	unsigned int hour;	// 24 hour format
     double min;

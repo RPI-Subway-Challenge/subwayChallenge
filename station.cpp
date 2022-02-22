7a5dec747f7ad8c5ff4d58384ca22590cd519d61
Mason duBoef <mduboef@gmail.com>
2022-02-21 21:35:42 -0500

    Add getCords & change lines to pairs

--- station.cpp
+++ station.cpp
@@ -6,7 +6,7 @@ Station::Station(){
 	name = "NONE";
 }
 
-Station::Station(int inId, const std::string& inName, double lat, double lon, const std::list<std::string>& inLines){
+Station::Station(int inId, const std::string& inName, double lat, double lon, const std::list<std::pair<std::string,int>>& inLines){
 	id = inId; // pop
 	name = inName;
 	cords.first = lat;
@@ -31,12 +31,12 @@ void Station::setTrips(const std::list<Trip>& inTrips){
 std::ostream& operator<<(std::ostream& os, const Station& s){
     os << s.getId() << " | ";
     os << s.getName()<< " | ";
-    os << s.getCords() << std::endl;
+    os << s.getCordStr() << std::endl;
 
     return os;
 }
 
-std::string Station::getCords() const{
+std::string Station::getCordStr() const {
     return "(" + std::to_string(cords.first) + ", " + std::to_string(cords.second) + ")";
 }
 
--- station.h
+++ station.h
@@ -16,26 +16,28 @@ public:
 	Station();
 
 	// constructor
-	Station(int id, const std::string& inName, double lon, double LAT, const std::list<std::string>& inLines);
-    Station(int id, const std::string& inName, double lon, double lat);
+	Station(int id, const std::string& inName, double lon, double LAT, const std::list<std::pair<std::string,int>>& inLines);
+    Station(int id, const std::string& inName, double lon, double lat);		// main contructor
 
 	// setters
 	void setTrips(const std::list<Trip>& inTrips);
+	// TODO addLine fuction
 
 	// getters
-	int getId() const 	 	 				    {return id;}
-	std::string getName() const 		        {return name;}
-    std::list<std::string> getLines() const	    {return lines;}
-    std::list<Trip> getTrips() const			{return trips;}
-	bool getOpen() const						{return open;}
-
-    std::string getCords() const;
+	int getId() const 	 	 				    				{return id;}
+	std::string getName() const 		        				{return name;}
+    std::list<std::pair<std::string,int>> getLines() const	    {return lines;}
+    std::list<Trip> getTrips() const							{return trips;}
+	bool getOpen() const										{return open;}
+    std::pair <double,double> getCords() const					{return cords;}
+	
+	std::string getCordStr() const;
 
 private:
 	int id;
 	std::string name;
 	std::pair <double,double> cords;
-	std::list<std::string> lines;
+	std::list<std::pair<std::string,int>> lines;
 	std::list<Trip> trips;
 	bool open;
 	// ? possibly operating hours

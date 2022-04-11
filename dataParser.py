f9c699c734cd109ad947fd1ba235a28431210658
Mason duBoef <mduboef@gmail.com>
2022-04-11 11:01:16 -0400

    Delete dataParser in subwayChallenge Repo

--- dataParser.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def dataParser(filename):
-    file = open(filename, 'r')
-    reader = csv.reader(file)
-    data = []
-    for row in reader:
-        data.append(row)
-    file.close()

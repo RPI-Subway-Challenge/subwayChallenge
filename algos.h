e51f6c276689f93c5ebf060f7d6625a27333d588
Mason duBoef <mduboef@gmail.com>
2022-10-04 16:53:47 -0400

    Revert

--- algos.h
+++ algos.h
@@ -5,5 +5,5 @@
 #ifndef SUBWAYCHALLENGE_ALGOS_H
 #define SUBWAYCHALLENGE_ALGOS_H
 double realDistance(const Station& s1,const Station& s2);
-int heuristic(std::vector<Station> & stations, int currId, int nextId);
+int heuristic(std::vector<Station> & stations, int id);
 #endif //SUBWAYCHALLENGE_ALGOS_H

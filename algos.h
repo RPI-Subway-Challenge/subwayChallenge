73c81929d5882d40413337bae64a700271f27db9
MagentaTreehouse <chenm16@rpi.edu>
2022-11-03 13:45:14 -0400

    Initialize edgeTo and costTo
    Handle different types of edge and cost tables

--- algos.h
+++ algos.h
@@ -7,6 +7,9 @@
 
 #include <vector>
 #include <queue>
+#include <limits>
+#include <concepts>
+#include <unordered_map>
 #include "station.h"
 
 double realDistance(const Station& s1, const Station& s2);
@@ -18,31 +21,41 @@ int heuristic(std::vector<Station> & stations, int currId, int nextId);
 // This function perform the move from one station to another
 Station* moveByTrip(std::vector<Station>& stations, Station* startStation, Trip& tripTaken, int& currentTime);
 
-template<class EdgeMap, class CostMap>
+template<class EdgeTab, class CostTab>
 struct UCSResult {
-    EdgeMap edgeTo;
-    CostMap costTo;
+    EdgeTab edgeTo;
+    CostTab costTo;
 };
+
+template<class T>
+concept MapT =
+    requires(T m, typename T::key_type k) {
+        {m[k]} -> std::same_as<typename T::mapped_type &>;
+    };
 
 // helper for the uniform cost search routine
 template<
     class GraphT,
     class NodeT,
-    class EdgeMap,  /* map[NodeT] -> EdgeT */
-    class CostMap,  /* map[NodeT] -> CostT */
+    class EdgeTab,  /* Edge table: map[NodeT] -> EdgeT */
+    class CostTab,  /* Cost table: map[NodeT] -> CostT */
     class PQT       /* priority queue */
 >
-void ucs(const GraphT &g, const NodeT &src, EdgeMap &edgeTo, CostMap &costTo, PQT &pq) {
+void ucs(const GraphT &g, const NodeT &src, EdgeTab &edgeTo, CostTab &costTo, PQT &pq) {
     costTo[src] = 0;
     pq.push(src);
     do {
-        auto n{pq.top()};
-        pq.pop();
+        auto n{std::move(pq.extract().value())};
         for (const auto &edge: g[n].neighbors()/* TODO */) {
             auto successor{edge.head()/* TODO */};
-            auto d{costTo[successor]},
-                newCost{costTo[n] + edge.val()};
-            if (/* TODO d indicates "not visited" ||*/ newCost < d) {
+            auto newCost{costTo[n] + edge.val()};
+            if ([&] () {
+                    if constexpr (MapT<CostTab>) {
+                        if (!costTo.contains(successor)) return true;
+                    }
+                    return newCost < costTo[successor];
+                } ()
+            ) {
                 edgeTo[successor] = edge;
                 if (pq.contains(successor)/* TODO */) {
                     pq.decreaseKey(successor, newCost)/* TODO */;
@@ -61,13 +74,29 @@ void ucs(const GraphT &g, const NodeT &src, EdgeMap &edgeTo, CostMap &costTo, PQ
  * 
  * @tparam GraphT the graph data structure
  * @tparam NodeT the node type
- * @tparam EdgeMap map[NodeT] -> EdgeT
- * @tparam CostMap map[NodeT] -> CostT
+ * @tparam EdgeTab edge table. map[NodeT] -> EdgeT.
+ * When NodeT is size_t, std::vector is recommended.
+ * @tparam CostTab cost table. map[NodeT] -> CostT.
+ * When NodeT is size_t, std::vector is recommended.
  */
-template<class GraphT, class NodeT, class EdgeMap, class CostMap>
-UCSResult<EdgeMap, CostMap> ucs(const GraphT &g, const NodeT &src) {
-    EdgeMap edgeTo; /* TODO initialize */
-    CostMap costTo; /* TODO initialize */
+template<
+    class GraphT,
+    class NodeT,
+    class EdgeTab = std::unordered_map<NodeT, typename GraphT::EdgeT>,
+    class CostTab = std::unordered_map<NodeT, typename GraphT::EdgeValT>
+>
+UCSResult<EdgeTab, CostTab> ucs(const GraphT &g, const NodeT &src) {
+    using ValT = CostTab::value_type;
+    auto makeTable{[&] <class TabType> (TabType) {
+        if constexpr (MapT<TabType>)
+            return TabType{};
+        else if constexpr (std::floating_point<ValT>)
+            return TabType(g.order(), std::numeric_limits<ValT>::infinity());
+        else
+            return TabType(g.order(), std::numeric_limits<ValT>::max());
+    }};
+    auto edgeTo{makeTable(EdgeTab{})};
+    auto costTo{makeTable(CostTab{})};
     std::priority_queue<NodeT, std::vector<NodeT>,
         decltype([&] (const NodeT &n1, const NodeT &n2) {
             return costTo[n1] < costTo[n2];

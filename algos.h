b8658c9a7ffc212bdefe3b0059cc760f620cfb86
MagentaTreehouse <chenm16@rpi.edu>
2022-11-09 21:22:45 -0500

    Added Graph concept
    Introduced the ucs namespace

--- algos.h
+++ algos.h
@@ -25,48 +25,70 @@ int heuristic(std::vector<Station> & stations, int currId, int nextId);
 // This function perform the move from one station to another
 Station* moveByTrip(std::vector<Station>& stations, Station* startStation, Trip& tripTaken, int& currentTime);
 
+template<class T>
+concept Map =
+    requires (T m, typename T::key_type k) {
+        {m[k]} -> std::same_as<typename T::mapped_type &>;
+    };
+
+/* uniform-cost search */
+namespace ucs {
+
+template<class T>
+concept Graph = 
+    requires (T g, typename T::NodeKeyT nk, typename T::EdgeKeyT ek) {
+        {g.order()} -> std::convertible_to<size_t>;
+        {g[nk]} -> std::convertible_to<typename T::NodeT>;
+        {g[ek]} -> std::convertible_to<typename T::EdgeT>;
+        {g[nk].neighbors()} -> std::ranges::range; /* TODO */
+        {*g[nk].neighbors().begin()} -> std::convertible_to<typename T::EdgeKeyT>;
+        {g[ek].head()} -> std::convertible_to<typename T::NodeKeyT>; /* TODO */
+    };
+
 template<class NodeKeyT, class EdgeTab, class CostTab>
-struct UCSResult {
+struct Result {
     NodeKeyT minCostNodeKey;
     EdgeTab edgeTo;
     CostTab costTo;
 };
 
-template<class T>
-concept MapT =
-    requires(T m, typename T::key_type k) {
-        {m[k]} -> std::same_as<typename T::mapped_type &>;
-    };
-
-// helper for the uniform cost search routine
-template<class GraphT, class NodeKeyT, class EdgeTab, class CostTab, class PQT>
-constexpr auto ucs(
-    const GraphT &g, NodeKeyT src, EdgeTab &edgeTo, CostTab &costTo, PQT &pq,
-    auto costFunc
+/**
+ * helper for the uniform cost search routine
+ * modifies: edgeTo, costTo
+ * @returns the node key of the destination with minimum cost
+ */
+template<Graph G, class NodeKeyT, class EdgeTab, class CostTab>
+constexpr auto findMinCost(
+    const G &g, NodeKeyT src, EdgeTab &edgeTo, CostTab &costTo, auto costFunc
 ) {
+    auto cmp{[&] (const NodeKeyT &nk0, const NodeKeyT &nk1) {
+        return costTo[nk0] < costTo[nk1];
+    }};
+    std::priority_queue<NodeKeyT, std::vector<NodeKeyT>, decltype(cmp)> pq{cmp};
     costTo[src] = 0;
-    pq.push(std::move(src));
-    NodeKeyT minCostNK; /* TODO init and update */
+    pq.push(src);
+    auto minCostNK{std::move(src)};
     do {
         auto pqNode{pq.extract()};
         const auto &nk{pqNode.value()};
-        for (const auto &edge: g[nk].neighbors()/* TODO */) {
-            const auto &successor{edge.head(NodeKeyT{})/* TODO */};
-            auto newCost{costTo[nk] + edge.val()};
-            if ([&] () {
-                    if constexpr (MapT<CostTab>) {
-                        if (!costTo.contains(successor)) return true;
+        for (const auto &edgeKey: g[nk].neighbors()) {
+            auto newCost{costTo[nk] + costFunc(g[edgeKey])};
+            if (NodeKeyT &&successorKey{g[edgeKey].head()};
+                [&] () {
+                    if constexpr (Map<CostTab>) {
+                        if (!costTo.contains(successorKey)) return true;
                     }
-                    return newCost < costTo[successor];
+                    return newCost < costTo[successorKey];
                 } ()
             ) {
-                edgeTo[successor] = edge;
-                if (pq.contains(successor)/* TODO */) {
-                    pq.decreaseKey(successor, newCost)/* TODO */;
+                if (pq.contains(successorKey)/* TODO */) {
+                    pq.decreaseKey(successorKey, newCost)/* TODO */;
                 } else {
-                    costTo[successor] = newCost;
-                    pq.push(std::move(successor));
+                    costTo[successorKey] = newCost;
+                    pq.push(successorKey);
                 }
+                minCostNK = std::move(successorKey);
+                edgeTo[minCostNK] = edgeKey;
             }
         }
     } while (!pq.empty());
@@ -74,61 +96,66 @@ constexpr auto ucs(
 }
 
 /**
- * @brief The uniform cost search. Finds the node with the lowest cost along with the
+ * @brief A uniform-cost search. Finds the node with the lowest cost along with the
  * cost to all other nodes and the path to them from a specific starting node.
  * 
- * @tparam GraphT the graph data structure
+ * @tparam G the graph data structure
  * @tparam NodeKeyT the type of the key to index into the graph to access the node.
- * Highly recommended to be trivially copiable. GraphT[NodeKeyT] -> NodeT.
- * @tparam EdgeTab edge table. map[NodeKeyT] -> EdgeT. Defaults to std::unordered_map,
- * unless when src is std::size_t it defaults to dynamic array of size g.order().
+ * Highly recommended to be trivially copiable. G[NodeKeyT] -> NodeT. Note: if you use
+ * std::size_t for this type, it is assumed by default that it can be used to index into
+ * an array of size g.order().
+ * @tparam EdgeTab table of edge keys. map[NodeKeyT] -> EdgeKeyT. Defaults to
+ * std::unordered_map, unless when src is std::size_t it defaults to dynamic array of
+ * size g.order().
  * @tparam CostTab cost table. map[NodeKeyT] -> CostT. Defaults to std::unordered_map,
  * unless when src is std::size_t it defaults to dynamic array of size g.order().
  * 
  * @param g the input graph
  * @param src key to the source node. The referenced node must exist in the graph.
- * @param costFunc the function object to calculate the cost of an edge
+ * @param costFunc the function object to calculate the cost of an edge. It should take
+ * an edge, preferably by const &, and returns the cost. Recommended example signature:
+ * G::EdgeValT (const G::EdgeT &edge).
  */
 template<
-    class GraphT, class NodeKeyT,
-    class EdgeTab = std::conditional_t<std::same_as<NodeKeyT, size_t>,
-        std::unique_ptr<typename GraphT::EdgeT []>,
-        std::unordered_map<NodeKeyT, typename GraphT::EdgeT>
+    Graph G,
+    class EdgeTab = std::conditional_t<std::same_as<typename G::NodeKeyT, size_t>,
+        std::unique_ptr<typename G::EdgeKeyT []>,
+        std::unordered_map<typename G::NodeKeyT, typename G::EdgeKeyT>
     >,
-    class CostTab = std::conditional_t<std::same_as<NodeKeyT, size_t>,
-        std::unique_ptr<typename GraphT::EdgeValT []>,
-        std::unordered_map<NodeKeyT, typename GraphT::EdgeValT>
+    class CostTab = std::conditional_t<std::same_as<typename G::NodeKeyT, size_t>,
+        std::unique_ptr<typename G::EdgeValT []>,
+        std::unordered_map<typename G::NodeKeyT, typename G::EdgeValT>
     >
 >
-constexpr auto ucs(
-    const GraphT &g, const NodeKeyT &src,
-    auto costFunc = [] (const GraphT::EdgeT &e) {return e.val();}
-) {
+constexpr auto search(
+    const G &g, const typename G::NodeKeyT &src,
+    auto costFunc = [] (const G::EdgeT &e) {return e.val();}
+) requires /*std::copy_constructible<NodeKeyT> && std::movable<NodeKeyT> &&*/
+std::same_as<decltype(costFunc(typename G::EdgeT{})), typename G::EdgeValT> {
     auto edgeTo{[&] () {
-        if constexpr (MapT<EdgeTab>)
+        if constexpr (Map<EdgeTab>)
             return EdgeTab{};
-        return std::make_unique<typename GraphT::EdgeT []>(g.order());
+        return std::make_unique<typename G::EdgeKeyT []>(g.order());
     } ()};
     auto costTo{[&] () {
-        if constexpr (MapT<CostTab>)
+        if constexpr (Map<CostTab>)
             return CostTab{};
         using ValT = CostTab::element_type;
-        auto p{std::make_unique_for_overwrite<typename GraphT::EdgeValT []>(g.order())};
-        static_assert(std::is_arithmetic_v<ValT>, "Unsupported value type for cost");
+        auto p{std::make_unique_for_overwrite<typename G::EdgeValT []>(g.order())};
+        static_assert(std::is_arithmetic_v<ValT>,
+            "Unsupported value type for cost, i.e., the edge value type");
         if constexpr (std::integral<ValT>)
             std::fill_n(p, g.order(), std::numeric_limits<ValT>::max());
         else if constexpr (std::floating_point<ValT>)
             std::fill_n(p, g.order(), std::numeric_limits<ValT>::infinity());
         return p;
     } ()};
-    std::priority_queue<NodeKeyT, std::vector<NodeKeyT>,
-        decltype([&] (const NodeKeyT &nk0, const NodeKeyT &nk1) {
-            return costTo[nk0] < costTo[nk1];
-        })>
-        pq;
-    return UCSResult{ /* TODO make sure evaluation order is correct! */
-        ucs(g, src, edgeTo, costTo, pq, costFunc), std::move(edgeTo), std::move(costTo)
+    return Result{ /* TODO make sure evaluation order is correct! */
+        findMinCost(g, src, edgeTo, costTo, costFunc), std::move(edgeTo),
+        std::move(costTo)
     };
+}
+
 }
 
 #endif //SUBWAYCHALLENGE_ALGOS_H

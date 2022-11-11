911ab5db8554f98661bf9a10ae4cd0fa84d653ff
Mason duBoef <mduboef@gmail.com>
2022-11-11 16:45:43 -0500

    Add files via upload

--- algos.cpp
+++ algos.cpp
@@ -137,7 +137,7 @@ std::vector<int> testAlg(std::vector<Station> stations, std::vector<int> goal,
   int start){
     //Minimum cost up to goal from root
     std::vector<int> answer;
-    for (long unsigned int i = 0; i != goal.size(); i++) {
+    for (long unsigned int i = 0; i < goal.size(); i++) {
         answer.push_back(INT_MAX);
     }
     //Queue stores cumulative distance and station id
@@ -145,7 +145,7 @@ std::vector<int> testAlg(std::vector<Station> stations, std::vector<int> goal,
     //map stores station id and bit representing if it was visited
     //0 = unvisited, 1 = visited
     std::map<int,int> visited;
-    for (long unsigned int i = 0; i != stations.size(); i++) {
+    for (long unsigned int i = 0; i < stations.size(); i++) {
         visited.insert({stations[i].getId(), 0});
     }
 
